6b09507124e345999dec497eef216b3e11a4d298
superdrivee <soojeen.sin@connect.qut.edu.au>
2023-04-11 10:21:16 +1000

    Chat Completed

--- src/server.js
+++ src/server.js
@@ -15,14 +15,19 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+function onSocketClose() {
+  console.log("Disconnected from the Browser ❌");
+}
+
+const sockets = [];
+
 wss.on("connection", (socket) => {
+  sockets.push(socket);
   console.log("Connected to Browser ✅");
-  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("close", onSocketClose);
   socket.on("message", (message) => {
-    const translatedMessageData = message.toString("utf8");
-    console.log(translatedMessageData);
+    sockets.forEach((aSocket) => aSocket.send(message.toString()));
   });
-  socket.send("hello!!!");
 });
 
 server.listen(3000, handleListen);
